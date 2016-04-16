47a56389003f8a7d2eeaac9bef083278faf0feb8
kiyonori <kiyonori.matsumoto@gmail.com>
2016-04-17 01:49:07 +0900

    add oumugaesi callback

--- app/controllers/line_collbacks_controller.rb
+++ app/controllers/line_collbacks_controller.rb
@@ -1,4 +1,29 @@
+require 'json'
+require 'rest-client'
+
 class LineCollbacksController < ApplicationController
-  def create
+  skip_before_filter :verify_authenticity_token
+
+  def callback
+    params['result'].each do |msg|
+      request_content = {
+        to: [msg['content']['from']],
+        to_Channel: 1383378250, #fixed valule
+        eventType: '138311608800106203',
+        content: msg['content']
+      }
+
+      endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+      content_json = request_content.to_json
+
+      RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+      RestClient.post(endpoint_uri, contenT_json, {
+        'Content-Type' => 'application/jeon; charset=UTF-8',
+        'X-Line-ChannelID' => ENV['LINE_CHANNEL_ID'],
+        'X-Line-ChannelSecret' => ENV['LINE_CHANNEL_SECRET'],
+        'X-Line-Trusted-User-With-ACL' => ENV['LINE_CHANNEL_MID']
+      })
+    end
+    "OK"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'line_collbacks/create'
+  post 'callback' => 'line_collbacks#callback'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
